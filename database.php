4fb6877f67cbb73cdfe8ab111dcf90f284025d12
o0Kazuma0o <blkhuletz25@gmail.com>
2025-02-26 11:20:30 +0800

    changes

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Enable exception for errors
 
-$environment = 'production';
+$environment = 'local';
 
 if ($environment === 'local') {
     $conn = new mysqli('localhost', 'root', '', 'bcp-sms_admission');
