c7350d0550554f3996bc4a545ab0460b2dcdca88
o0Kazuma0o <blkhuletz25@gmail.com>
2025-03-03 11:02:55 +0800

    Revert "adding ai"
    This reverts commit 2c5805eb8a8ed0ac1af795c88a25cd385bb8cad1.

--- admin/Dashboard.php
+++ admin/Dashboard.php
@@ -291,6 +291,13 @@ if ($result_enrollment_status) {
       </li>
 
       <li class="nav-item">
+        <a class="nav-link " href="admission_temp.php">
+          <i class="bi bi-grid"></i>
+          <span>Temporary Admission</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
         <a class="nav-link " href="enrollment.php">
           <i class="bi bi-grid"></i>
           <span>Enrollment</span>
